bd9a4672d45345daf695bbfb6e78a1496be84145
SiddhardhSan <sanivadasiddhardh01@gmail.com>
2025-09-30 16:06:43 +0530

    Learning DOM javascript ,window object, DOM-manupulation (properties like tagName,innerHTML,innerText,querySelector etc)

--- functionsJS.js
+++ functionsJS.js
@@ -29,11 +29,53 @@ newarrf.forEach((i)=>{console.log(i);});
 console.log("\n");
 
 
-//reduce
+//reduce (previous,current)
 let maxx=arrf.reduce((i,j)=>{
     
     return ((i>j)? i:i=j);
 });
 console.log(maxx);
 
+//                                        Learning document of object model
+let q= document.querySelectorAll("a");
+console.dir(q);
+console.log(q);
+let di=document.querySelector("body");
+console.log(di.innerText);
+console.log(di.innerHTML);
+console.log(q[1].innerText);
+console.log(q[1].innerHTML);
+console.log(di.tagName);
+console.log(q[3].tagName);
 
+let kl=document.body.children;
+console.log(di.firstChild);
+let s1=document.querySelector(".b1");
+console.log(s1.innerText,"\n");
+// console.log("\n");
+console.log(s1.innerHTML);
+let nl=s1.children;
+console.log(nl);
+console.log(nl[1].innerText);
+
+// more ways of using document of object model - javascript
+
+let a1=document.createElement("p");
+(document.querySelector("#bp1")).after(a1);
+a1.innerText="Please try to contribute sources";
+a1.setAttribute("id","bp4");
+a1.style.marginTop= "0.37rem";
+
+let new1=document.querySelector(".b1");
+let news1=document.createElement("div");
+new1.after(news1);
+news1.setAttribute("class","b2");
+news1.innerHTML=`
+<p>Thank you everyone</p>
+    <p>please try to stay in touch</p>
+`;
+console.log(news1.classList);
+news1.classList.add("newClass");
+news1.remove();
+// news1=null;
+// console.log(news1.classList);
